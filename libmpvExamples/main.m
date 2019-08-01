cf280bf3be2102140e4bbcf56c09acdc23693c82
digital-pers0n <aquaphonic@gmail.com>
2019-08-01 19:08:57 +0300

    Load OpenGL.framework

--- libmpvExamples/main.m
+++ libmpvExamples/main.m
@@ -7,7 +7,17 @@
 //
 
 #import <Cocoa/Cocoa.h>
+#import <dlfcn.h>
 
 int main(int argc, const char * argv[]) {
+    
+    void *g_opengl_framework_handle;
+    g_opengl_framework_handle = dlopen("/System/Library/Frameworks/OpenGL.framework/OpenGL",
+                                       RTLD_LAZY | RTLD_LOCAL);
+    if (!g_opengl_framework_handle) {
+        fprintf(stderr, "Cannot load OpenGL.framework\n");
+        return EXIT_FAILURE;
+    }
+    
     return NSApplicationMain(argc, argv);
 }

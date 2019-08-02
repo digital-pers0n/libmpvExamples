d279e2fa93be5e8d80c32e15b6ec2013a822e6c3
digital-pers0n <aquaphonic@gmail.com>
2019-08-02 18:00:49 +0300

    Add header file

---
+++ libmpvExamples/libmpvExamples.h
@@ -0,0 +1,20 @@
+//
+//  libmpvExamples.h
+//  libmpvExamples
+//
+//  Created by Terminator on 2019/08/02.
+//  Copyright © 2019年 home. All rights reserved.
+//
+
+#ifndef libmpvExamples_h
+#define libmpvExamples_h
+
+/* CocoaCB */
+#import "CocoaCB/CocoaCB.h"
+#import "CocoaCB/EventsView.h"
+#import "CocoaCB/Window.h"
+#import "CocoaCB/MPVHelper.h"
+#import "CocoaCB/VideoLayer.h"
+
+
+#endif /* libmpvExamples_h */

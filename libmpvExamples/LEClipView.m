2e7d6a688f44f437688e0b3f68e8bba39b6ea3f5
digital-pers0n <aquaphonic@gmail.com>
2019-08-02 18:27:25 +0300

    Add NSClipView subclass

---
+++ libmpvExamples/LEClipView.m
@@ -0,0 +1,17 @@
+//
+//  LEClipView.m
+//  libmpvExamples
+//
+//  Created by Terminator on 2019/08/02.
+//  Copyright © 2019年 home. All rights reserved.
+//
+
+#import "LEClipView.h"
+
+@implementation LEClipView
+
+- (BOOL)isFlipped {
+    return YES;
+}
+
+@end

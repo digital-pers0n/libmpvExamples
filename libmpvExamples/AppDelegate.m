2426bb7cec506f618e4b4f1a50364306b71b55ca
digital-pers0n <aquaphonic@gmail.com>
2019-08-03 19:46:06 +0300

    Set up main window

--- libmpvExamples/AppDelegate.m
+++ libmpvExamples/AppDelegate.m
@@ -12,16 +12,69 @@
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+@property NSURL *fileURL;
+@property id currentExample;
+
 @end
 
 @implementation AppDelegate
 
+#pragma mark - NSApplicationDelegate
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+    _currentExample = nil;
 }
+
+- (BOOL)application:(NSApplication *)sender openFile:(NSString *)filename {
+    _fileURL = [NSURL fileURLWithPath:filename];
+    _window.representedURL = _fileURL;
+    return YES;
+}
+
+#pragma mark - Methods
+
+- (NSURL *)selectFile {
+    NSOpenPanel *openPanel = NSOpenPanel.openPanel;
+    openPanel.allowedFileTypes = @[@"mkv", @"mp4", @"avi", @"m4v", @"mov", @"3gp", @"ts", @"mts", @"m2ts", @"wmv", @"flv", @"f4v", @"asf", @"webm", @"rm", @"rmvb", @"qt", @"dv", @"mpg", @"mpeg", @"mxf", @"vob", @"gif"];
+    if (openPanel.runModal == NSModalResponseOK) {
+        return openPanel.URL;
+    }
+    return nil;
+}
+
+#pragma mark - IBAction methods
+
+- (IBAction)runCocoaCBExample:(id)sender {
+    if (_currentExample) {
+        _currentExample = nil;
+    }
+    
+    if (!_fileURL) {
+        if (!(_fileURL = [self selectFile])) {
+            NSBeep();
+            return;
+        }
+    } else {
+        _window.representedURL = _fileURL;
+        CocoaCB *ccb = CocoaCB.new;
+        const char *args[] = { "loadfile", _fileURL.fileSystemRepresentation, NULL };
+        mpv_command(ccb.mpv.mpv_handle, args);
+        _currentExample = ccb;
+    }
+}
+
+- (void)openDocument:(id)sender {
+    NSURL *filename = [self selectFile];
+    if (filename) {
+        _fileURL = filename;
+        _window.representedURL = filename;
+        [NSDocumentController.sharedDocumentController noteNewRecentDocumentURL:filename];
+    }
+}
+
 
 @end

0514c0b10d5c1b479d2bf6cca10b3626ca7ce78f
digital-pers0n <aquaphonic@gmail.com>
2019-08-23 17:04:01 +0300

    Add files

---
+++ libmpvExamples/MPVPlayer/MPVPlayerCommands.h
@@ -0,0 +1,20 @@
+//
+//  MPVPlayerCommands.h
+//  libmpvExamples
+//
+//  Created by Terminator on 2019/08/23.
+//  Copyright © 2019年 home. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#ifndef MPVPlayerCommands_h
+#define MPVPlayerCommands_h
+
+/** loadfile */
+extern NSString * const MPVPlayerCommandLoadFile;
+
+/** stop */
+extern NSString * const MPVPlayerCommandStop;
+
+#endif /* MPVPlayerCommands_h */
---
+++ libmpvExamples/MPVPlayer/MPVPlayerCommands.m
@@ -0,0 +1,12 @@
+//
+//  MPVPlayerCommands.m
+//  libmpvExamples
+//
+//  Created by Terminator on 2019/08/23.
+//  Copyright © 2019年 home. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NSString * const MPVPlayerCommandLoadFile       = @"loadfile";
+NSString * const MPVPlayerCommandStop           = @"stop";

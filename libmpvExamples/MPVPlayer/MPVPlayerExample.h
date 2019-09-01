608cbc02b79d3a97002b4e1aa9b29d4cc1b4cfc0
digital-pers0n <aquaphonic@gmail.com>
2019-09-01 18:02:54 +0300

    Add declarations

--- libmpvExamples/MPVPlayer/MPVPlayerExample.h
+++ libmpvExamples/MPVPlayer/MPVPlayerExample.h
@@ -8,6 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
+@class MPVPlayer, MPVPlayerWindow;
+
 @interface MPVPlayerExample : NSObject
 
+@property (readonly, nonatomic) MPVPlayer *player;
+@property (readonly, nonatomic) MPVPlayerWindow *window;
+
+- (void)shutdown;
+
 @end
+
+NS_ASSUME_NONNULL_END

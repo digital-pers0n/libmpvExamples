03ed74c01c6a3638c1e3a17d242b63cb30e85bcf
digital-pers0n <aquaphonic@gmail.com>
2019-09-30 18:53:50 +0300

    Add missing enum type

--- libmpvExamples/MPVPlayer/MPVPlayerExample.h
+++ libmpvExamples/MPVPlayer/MPVPlayerExample.h
@@ -11,6 +11,7 @@
 typedef NS_ENUM(NSInteger, MPVPlayerExampleType) {
     MPVPlayerExampleNSOpenGLView,
     MPVPlayerExampleNSView,
+    MPVPlayerExampleHybridView,
     MPVPlayerExampleCAOpenGLLayer
 };
 

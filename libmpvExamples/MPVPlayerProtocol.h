24a9f1b8a43b6e355527ea3fa8e78cbba1ca3b55
digital-pers0n <aquaphonic@gmail.com>
2021-05-29 21:25:20 +0300

    Add missing file

---
+++ libmpvExamples/MPVPlayerProtocol.h
@@ -0,0 +1,17 @@
+//
+//  MPVPlayerProtocol.h
+//  libmpvExamples
+//
+//  Created by Terminator on 2021/5/22.
+//  Copyright © 2021年 home. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol MPVPlayer <NSObject>
+- (void)loadURL:(NSURL*)url;
+- (void)shutdown;
+- (BOOL)isReadyToPlay;
+- (void)play;
+- (void)pause;
+@end

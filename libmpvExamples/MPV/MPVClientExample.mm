8c455e8a137a15f77208cc619be4b11b5a6ef2ac
digital-pers0n <aquaphonic@gmail.com>
2021-07-13 15:43:45 +0300

    Implement drag n drop functionality

--- libmpvExamples/MPV/MPVClientExample.mm
+++ libmpvExamples/MPV/MPVClientExample.mm
@@ -14,6 +14,32 @@
 #define MPV_CLIENT_EXAMPLE_USE_LEGACY_HWDEC 0
 #endif
 
+#if MAC_OS_X_VERSION_MIN_REQUIRED >= 110000
+#define MAC_OS_1100_BUILD 1
+#else
+#define MAC_OS_1100_BUILD 0
+#endif
+
+namespace {
+NSPasteboardType MPVPasteboardTypeFileURL() noexcept {
+#if MAC_OS_1100_BUILD
+    return NSPasteboardTypeFileURL;
+#else
+    return NSPasteboardType(kUTTypeFileURL);
+#endif
+}
+
+NSPasteboardType MPVPasteboardTypeURL() noexcept {
+#if MAC_OS_1100_BUILD
+    return NSPasteboardTypeURL;
+#else
+    return NSPasteboardType(kUTTypeURL);
+#endif
+}
+} // anonymous namespace
+
+@interface MPVClientExample () <NSWindowDelegate, NSDraggingDestination> @end
+
 @implementation MPVClientExample {
     MPVClient *_mpv;
 }
@@ -44,6 +70,9 @@
     win.releasedWhenClosed = NO;
     win.movableByWindowBackground = YES;
     win.contentView = ex;
+    [win registerForDraggedTypes:@[ MPVPasteboardTypeFileURL(),
+                                    MPVPasteboardTypeURL() ]];
+    win.delegate = self;
     _window = win;
     auto nc = [NSNotificationCenter defaultCenter];
     [nc addObserver:self selector:@selector(willClose:)
@@ -69,6 +98,32 @@
     [NSNotificationCenter.defaultCenter removeObserver:self];
     id<MPVVideoRenderer> view = _window.contentView;
     [view destroy];
+}
+
+//MARK:- NSDraggingDestination
+
+- (NSDragOperation)draggingEntered:(id<NSDraggingInfo>)sender {
+    auto pboard = sender.draggingPasteboard;
+    if ([pboard.types containsObject:MPVPasteboardTypeFileURL()]
+        || [pboard.types containsObject:MPVPasteboardTypeURL()]) {
+        return NSDragOperationCopy;
+    }
+    return NSDragOperationNone;
+}
+
+- (BOOL)performDragOperation:(id<NSDraggingInfo>)sender {
+    auto pb = sender.draggingPasteboard;
+    NSArray<NSURL*> *urls = [pb readObjectsForClasses:@[NSURL.class]
+                                              options:nil];
+    if (urls.count) {
+        NSURL *url = urls.firstObject;
+        if ([url isFileReferenceURL]) {
+            url = [url filePathURL];
+        }
+        [_mpv loadURL:url];
+        return YES;
+    }
+    return NO;
 }
 
 @end
